1466ad187dbfba7f71fb06316755079d7cfabfe6
Fumiya Sato <fumiya.sato.rop@gmail.com>
2022-01-20 00:04:53 +0900

    update vin extraction gui function

--- vin_extraction/vin_extraction.py
+++ vin_extraction/vin_extraction.py
@@ -1,117 +1,136 @@
+from logging import getLogger
 import pandas as pd
 import re
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
+from tkinter import messagebox
 import os
+import datetime
 
-g_filepath = "./sampledata.txt"
-g_vinlist  = []
-g_vinpd    = pd.DataFrame()
-g_colname  = "ＶＩＮ／プレート打刻情報"
+logger = getLogger(__name__)
+logger.disabled = False
 
+g_vinpd       = pd.DataFrame()
+g_column_list = []
+g_filepath    = ""
+g_colname     = "ＶＩＮ／プレート打刻情報"
+
+# Extract VIN list from select file
 def readVIN(filepath):
-    global g_vinlist, g_vinpd
+    global g_vinpd, g_column_list
     n = 0
     p = r'"(.*?)"'
     data = []
 
-    with open(filepath) as f:
-        for line in f:
-            if line[0] == "#":
-                continue
-            else:
-                n = n+1
-                # 正規表現でlistとして抽出
-                r = re.findall(p, line) 
-                if n == 1:
-                    column_list = r
+    try:
+        with open(filepath) as f:
+            for line in f:
+                if line[0] == "#":
+                    continue
                 else:
-                    data.append(r)
-            # if n > 3: break
+                    n = n+1
+                    # Extract as list using regular expression
+                    r = re.findall(p, line) 
+                    if n == 1:
+                        g_column_list = r
+                        logger.info("column list:" + " ".join(g_column_list))
+                    else:
+                        data.append(r)
 
         # Create pandas data frame and delete VIN duplicate elements. 
-        df = pd.DataFrame(data, columns=column_list).drop_duplicates()
-        print(df[g_colname])
+        df = pd.DataFrame(data, columns=g_column_list).drop_duplicates()
+
+        # columnlistbox = tk.Listbox(column_list, listvariable=tk.StringVar(value=g_column_list), height=15,selectmode="single")
+        # columnlistbox.pack()
 
         g_vinpd   = df[g_colname].copy()
-        g_vinlist = df[g_colname].tolist()
-        list_value = tk.StringVar(value=g_vinlist)
-        Listbox = tk.Listbox(frame_list, listvariable=list_value, height=15)
+        logger.info(g_vinpd)
+
+        # Create vin list box with scrollbar
+        scroll=tk.Scrollbar(frame_list)
+        scroll.pack(side=tk.RIGHT,fill="y")
+        list_value = tk.StringVar(value=df[g_colname].tolist())
+        Listbox = tk.Listbox(frame_list, listvariable=list_value, height=15,selectmode="extended",yscrollcommand=scroll.set)
         Listbox.pack(padx=10)
+        scroll["command"]=Listbox.yview
 
+    except FileNotFoundError as e: 
+        messagebox.showerror("File error", str(e))
+    except Exception as e: 
+        messagebox.showerror("Exception error", str(e))
 
-# ファイル指定
+# Select reading file
 def filedialog_clicked():
     global g_filepath
     iDir = os.path.abspath(os.path.dirname(__file__))
-    filepath = filedialog.askopenfilename(initialdir = iDir)
+    filepath = filedialog.askopenfilename(initialdir = iDir, filetypes=[('テキストファイル', '*.txt'), ('CSVファイル', '*.csv')])
     g_filepath = filepath
-    entry1.set(filepath)
+    entry.set(filepath)
     
     
-# 実行
+# Execute vin extraction
 def exeVINExtraction():
     readVIN(g_filepath)
 
-# エクスポート
+# Export to txt
 def export_clicked():
-    g_vinpd.to_csv('./test.csv', header=True)
-    print("Export完了")
+    now = datetime.datetime.now()
+    filename = './vinlist' + now.strftime('%Y%m%d_%H%M%S') + '.txt'
+    if g_vinpd.empty:
+        messagebox.showerror("Export error", "[エクスポートエラー] VIN情報を読み込んでください")
+    else :
+        g_vinpd.to_csv(filename, header=False, index=False)
+        messagebox.showinfo("Export complete", "エクスポートが完了しました")
+        logger.info('Export complete.')
 
 
-
-
-
+# main
 if __name__ == "__main__":
 
-    # rootの作成
+    # Create root tKinter module
     root = tk.Tk()
     root.title("VIN Extraction")
 
-    # Frameの作成
+    # Create main frame
     frame_select = ttk.Frame(root, padding=10)
     frame_select.grid(row=0, column=1, sticky=tk.W)
 
-    # 「ファイル参照」ラベルの作成
+    # Create "File Reference" label
     IDirLabel = ttk.Label(frame_select, text="ファイル", padding=(5, 2))
     IDirLabel.pack(side=tk.LEFT)
     
-    # 「ファイル参照」エントリーの作成
-    entry1 = tk.StringVar()
-    IDirEntry = ttk.Entry(frame_select, textvariable=entry1, width=30)
+    # Create "File Reference" entry
+    entry = tk.StringVar()
+    IDirEntry = ttk.Entry(frame_select, textvariable=entry, width=30)
     IDirEntry.pack(side=tk.LEFT)
 
-    # 「フォルダ参照」ボタンの作成
+    # Create "File Reference" button
     IDirButton = ttk.Button(frame_select, text="参照", command=filedialog_clicked)
     IDirButton.pack(side=tk.LEFT)
 
-    # Frameの作成
+    # Create operation buttom frame
     padx_exe = 10
     frame_exe = ttk.Frame(root, padding=10)
     frame_exe.grid(row=2,column=1,sticky=tk.W)
 
-    # 実行ボタンの設置
+    # Create exe button
     button1 = ttk.Button(frame_exe, text="実行", command=exeVINExtraction)
     button1.pack(fill = "x", padx=padx_exe, side = "left")
 
-    # 実行ボタンの設置
+    # Create export button
     button1 = ttk.Button(frame_exe, text="エクスポート", command=export_clicked)
     button1.pack(fill = "x", padx=padx_exe, side = "left")
 
-    # キャンセルボタンの設置
+    # Create cancel button
     button2 = ttk.Button(frame_exe, text=("閉じる"), command=quit)
     button2.pack(fill = "x", padx=padx_exe, side = "left")
     
-    # Frameの作成
+    # Create vin list display frame
     frame_list = ttk.Frame(root, padding=10, width=200, height=100)
     frame_list.grid(row=3,column=1,sticky=tk.W)
 
-    # VIN Listの作成
-    # list_value = tk.StringVar(value=g_vinlist)
-    # Listbox = tk.Listbox(frame_list, listvariable=list_value, height=15)
-    # Listbox.pack(padx=10)
-    
-
+    # column_list = ttk.Frame(root, padding=10, width=200, height=100)
+    # column_list.grid(row=3,column=1,sticky=tk.W)
 
     root.mainloop()
